c6d7d423fa9f459705c3cd8ecca7806ba501c21c
fMurugi <fionamurugi99@gmail.com>
2024-02-26 23:59:15 +0300

    add delete course logic

--- instustionsManagement/src/main/java/com/fiona/instustionsManagement/courses/CourseService.java
+++ instustionsManagement/src/main/java/com/fiona/instustionsManagement/courses/CourseService.java
@@ -4,9 +4,12 @@ import com.fiona.instustionsManagement.Exceptions.DuplicateCourseException;
 import com.fiona.instustionsManagement.Exceptions.ResourceNotFoundException;
 import com.fiona.instustionsManagement.Institutions.InstitutionsModel;
 import com.fiona.instustionsManagement.Institutions.InstitutionsService;
+import com.fiona.instustionsManagement.students.StudentsModel;
+import com.fiona.instustionsManagement.students.StudentsRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -14,19 +17,25 @@ import java.util.UUID;
 public class CourseService {
     private final CoursesRepository coursesRepository;
     private final InstitutionsService institutionsService;
+    private final StudentsRepository studentsRepository;
 
     public String addCourseToInstitution(UUID institutionId, CourseDTO courseDTO){
         InstitutionsModel institutionsModel =institutionsService.findInstitutionById(institutionId);
         String courseName = courseDTO.getCourseName();
 
-        if (institutionsModel.getCoursesModelList().stream().anyMatch(
-                course -> course.getCourseName().equals(courseName)
-        )){
+        List<CoursesModel> coursesModelList = institutionsModel.getCoursesModelList();
+
+
+        if (institutionsModel.getCoursesModelList().stream().anyMatch(course -> {
+            String name = course.getCourseName();
+            return name != null && name.equals(courseName);
+        })) {
             throw new DuplicateCourseException("A course with the same name already exists in the institution");
         }
 
+
         CoursesModel newCourse = CoursesModel.builder()
-                .courseName(courseDTO.courseName)
+                .courseName(courseDTO.getCourseName())
                 .institutionsModel(institutionsModel)
                 .build();
 
@@ -47,6 +56,20 @@ public class CourseService {
         }
         coursesModel.setCourseName(newCourseName);
         return "Course successfully edited";
+    }
+
+
+    public String deleteCourse(UUID courseId) {
+        CoursesModel course = coursesRepository.findById(courseId)
+                .orElseThrow(() -> new ResourceNotFoundException("Course not found with ID: " + courseId));
+
+        List<StudentsModel> students = studentsRepository.findByCoursesModel(course);
+        if (!students.isEmpty()) {
+            return "Cannot delete course. This course has students.";
+        }
+
+        coursesRepository.delete(course);
+        return "Course deleted successfully";
     }
 
     public CoursesModel findCourseById(UUID courseId){
